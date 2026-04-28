d39426bcb7faee3ce812a58aa9dcb79d4241e2d8
David <36444115+dawissl@users.noreply.github.com>
2026-04-28 13:16:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function generate() {
 
   const workplaceMap = {
     H1: "Budova H1 – Hradební 1029/2, 500 03 Hradec Králové",
-    H2: "Budova H2 – Hradecká 647, 500 03 Hradec Králové"
+    H2: "Budova H2 – Hradecká 647, 500 02 Hradec Králové"
   };
 
   let result = template
